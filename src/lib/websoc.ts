4c8aeb8f6d4d8b5aff0dcf4b37b7990a37c16c6a
Jacob Sommer <jacobgsommer@gmail.com>
2023-11-26 21:21:02 -0800

    feat: helper functions for getting relevant terms

--- src/lib/websoc.ts
+++ src/lib/websoc.ts
@@ -21,6 +21,13 @@ enum Quarter {
   SUMMER2 = '76'
 }
 
+const term = z.object({
+  year: z.number(),
+  quarter: z.nativeEnum(Quarter)
+});
+
+type Term = z.infer<typeof term>;
+
 export function formatTermReadable(term: string) {
   const [year, quarter] = term.split('-');
 
@@ -35,7 +42,57 @@ export function formatTermReadable(term: string) {
   return `${year} ${quarterName}`;
 }
 
-async function getRelevantTerms() {}
+function getRelevantTerms(): Term[] {
+  const today = new Date();
+  const latestTerm = getLatestTerm(today);
+  const relevantTerms = [latestTerm];
+
+  for (let i = 1; i < Object.keys(Quarter).length; i++) {
+    const sucessiveTerm = relevantTerms[i - 1];
+    relevantTerms.push(getPreviousTerm(sucessiveTerm));
+  }
+
+  return relevantTerms;
+}
+
+function getPreviousTerm(term: Term): Term {
+  const { year, quarter } = term;
+
+  if (quarter === Quarter.FALL) {
+    return { year, quarter: Quarter.SUMMER2 };
+  } else if (quarter === Quarter.SUMMER2) {
+    return { year, quarter: Quarter.SUMMER10WK };
+  } else if (quarter === Quarter.SUMMER10WK) {
+    return { year, quarter: Quarter.SUMMER };
+  } else if (quarter === Quarter.SUMMER) {
+    return { year, quarter: Quarter.SPRING };
+  } else if (quarter === Quarter.SPRING) {
+    return { year, quarter: Quarter.WINTER };
+  } else if (quarter === Quarter.WINTER) {
+    return { year: year - 1, quarter: Quarter.FALL };
+  } else {
+    throw new Error('Invalid quarter');
+  }
+}
+
+function getLatestTerm(date: Date): Term {
+  const month = date.getMonth();
+  const year = date.getFullYear();
+
+  if (month >= 9) {
+    return { year, quarter: Quarter.FALL };
+  } else if (month >= 8) {
+    return { year, quarter: Quarter.SUMMER2 };
+  } else if (month >= 6) {
+    return { year, quarter: Quarter.SUMMER10WK };
+  } else if (month >= 4) {
+    return { year, quarter: Quarter.SPRING };
+  } else if (month >= 1) {
+    return { year, quarter: Quarter.WINTER };
+  } else {
+    return { year: year - 1, quarter: Quarter.FALL };
+  }
+}
 
 export async function getSyllabi(course: Course) {
   const { Dept, CourseNum } = course;
