1895ed9e3691912f6eb58456356b09f4329608be
Jacob Sommer <jacobgsommer@gmail.com>
2024-03-10 20:07:43 -0700

    cast neon

--- packages/database/index.ts
+++ packages/database/index.ts
@@ -1,9 +1,9 @@
-import { neon } from '@neondatabase/serverless';
+import { NeonQueryFunction, neon } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
 import * as schema from './schema';
 
 export const dbClient = function(database_url: string) {
-  const sql = neon(database_url);
+  const sql = neon(database_url) as NeonQueryFunction<boolean, boolean>;
   const db = drizzle(sql, { schema });
   return db;
 }
