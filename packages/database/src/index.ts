686cad81ac070bb52b5e733721b28f7f7fed2573
Jacob Sommer <jacobgsommer@gmail.com>
2024-11-17 19:41:02 -0800

    Refactor db export

--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -1,11 +1,5 @@
-import { NeonQueryFunction, neon } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
-import * as schema from './schema';
 
-export const dbClient = function (database_url: string) {
-  const sql = neon(database_url) as NeonQueryFunction<boolean, boolean>;
-  const db = drizzle(sql, { schema });
-  return db;
-};
+export const dbClient = (databaseUrl: string) => drizzle(databaseUrl);
 
 export * from 'drizzle-orm';
